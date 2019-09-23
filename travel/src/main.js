f8c986f22f3822fdd8120271fc01951db195f39e
lxy123123 <2944620080@qq.com>
2019-09-23 14:45:00 +0800

    project init

--- travel/src/main.js
+++ travel/src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入fastclick模块，解决移动端的click300ms延时问题
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 引入一像素边框的解决文件（一像素边框是可能1像素的css对应的二倍屏下不止1像素）
+import './assets/styles/border.css'
+
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
+  // 挂载到id为#app这个div中，是在index.html中的
   el: '#app',
   router,
+  // 在es6中如果键值和值一样的话，可以简写键值就可以 
   components: { App },
+  // 渲染APP局部组件
   template: '<App/>'
 })
--- travel/src/router/index.js
+++ travel/src/router/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
 export default new Router({
+  // 一组路由配置项
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },
   ]
 })
